352380715716f48d800ac56c8d05b33fa91f634d
assinie <github@assinie.info>
2019-07-12 21:35:58 +0200

    Rewrite of the ls command

--- src/commands/ls.asm
+++ src/commands/ls.asm
@@ -1,197 +1,187 @@
+NUMBER_OF_COLUMNS_LS = 3
+
 .proc _ls
-    NUMBER_OF_COLUMNS_LS = 3
-    
     lda #NUMBER_OF_COLUMNS_LS+1
-    sta NUMBER_OF_COLUMNS ; used to do columns
+    sta NUMBER_OF_COLUMNS
 
     jsr _ch376_verify_SetUsbPort_Mount
-    cmp #$01
-    beq @error
-    jmp no_error_for_mouting
-@error:
-    rts
-no_error_for_mouting:
-    ; jump to all directories :)
-    jsr _cd_to_current_realpath_new
-    
+    bcc @ZZ0001
+        jsr _cd_to_current_realpath_new
+        ldx #$01
+        jsr _orix_get_opt
 
+        STRCPY ORIX_ARGV, BUFNOM
+
+        lda BUFNOM
+        bne @ZZ0002
+            lda #"*"
+            sta BUFNOM
+            lda #$00
+            sta BUFNOM+1
+
+        @ZZ0002:
+        jsr _ch376_set_file_name
+        jsr _ch376_file_open
+        ; Au retour, on peut avoir USB_INT_SUCCESS ou USB_INT_DISK_READ)
+
+        ; $14 -> Fichier existant (USB_INT_SUCCESS) (cas 'ls fichie.ext')
+        ; $1D -> Lecture OK (USB_INT_DISk_READ
+        ; $41 -> Fin de liste (ERR_OPEN_DIR) ou ouverture répertoire (cas 'ls repertoire')
+        ; $42 -> fichier inexistant (ERR_MISS_FILE)
+
+        cmp #CH376_ERR_MISS_FILE
+        beq Error
+
+        @ZZ1001:
+        cmp #CH376_USB_INT_SUCCESS
+        bne @ZZ1002
+            lda #COLOR_FOR_FILES
+            bne display_one_file_catalog
+
+        @ZZ1002:
+        cmp #CH376_ERR_OPEN_DIR
+        bne @ZZ0003
+            lda #COLOR_FOR_DIRECTORY
+            bne display_one_file_catalog
+
+        ; cmp #CH376_USB_INT_SUCCESS
+        ; bne @ZZ0003
+        ;    lda #CH376_USB_INT_DISK_READ
+
+        @ZZ0003:
+            cmp #CH376_USB_INT_DISK_READ
+            bne @ZZ0004
+                lda #CH376_RD_USB_DATA0
+                sta CH376_COMMAND
+                lda CH376_DATA
+                cmp #32
+                beq @ZZ0005
+                    rts
+
+                @ZZ0005:
+                jsr display_catalog
+
+        ; display_one_file_catalog renvoie la valeur de _ch376_wait_response qui renvoie 1 en cas d'erreur
+        ; et le CH376 ne renvoie pas de valeur 0
+        ; donc le bne devient un saut inconditionnel!
+        ; jmp @ZZ0003
+        bne @ZZ0003
+
+        @ZZ0004:
+        BRK_ORIX XCRLF
+
+    @ZZ0001:
+    rts
+
+; ------------------------------------------------------------------------------
+Error:
+    PRINT txt_file_not_found
     ldx #$01
     jsr _orix_get_opt
-    STRCPY ORIX_ARGV,BUFNOM
-
-no_param:
-    
-    lda BUFNOM
-    bne ispattern
-  
-    lda #'*'
-    sta BUFNOM
-
-
-.IFPC02
-.pc02
-    stz BUFNOM+1
-.p02    
-.else
-	lda #$00
-	sta BUFNOM+1
-.endif
-  
-ispattern:
-    jsr _ch376_set_file_name
-	
-;***************************************************************************** opening (root) */
-	
-    jsr _ch376_file_open
-	 
-next_entry:
-    cmp #CH376_USB_INT_SUCCESS ;int_success
-    beq fin_catalogue
-    cmp #CH376_USB_INT_DISK_READ
-    bne fin_catalogue
-
-    LDA #CH376_RD_USB_DATA0 ;$27
-    STA CH376_COMMAND
-    lda CH376_DATA ;fetch the length 
-    cmp #32
-    beq catalogue_ok
-
-out_ls:
-    rts
-catalogue_ok:
-    jsr display_one_file_catalog
-.IFPC02
-.pc02
-    bra next_entry
-.p02
-.else
-    jmp next_entry
-.endif
-
-fin_catalogue:
-    BRK_TELEMON XCRLF  ;jump a line
-    rts
+    .BYTE $2C
 
 display_one_file_catalog:
+    .BYTE $00, XWR0
+    PRINT BUFNOM
+    BRK_ORIX XCRLF
+rts
+
+; ------------------------------------------------------------------------------
+
+display_catalog:
     lda #COLOR_FOR_FILES
     sta BUFNOM
-
     ldy #$01
     ldx #$01
-loop12:
 
-    lda CH376_DATA ; Fetch char
-    cmp #' '
-    beq no_need_to_display_space
-    jsr _lowercase_char
-    sta BUFNOM,y ; we store it
-    iny
+    @ZZ0007:
+        lda CH376_DATA
+        cmp #' '
+        beq @ZZ0008
+            jsr _lowercase_char
+            sta BUFNOM,y
+            iny
 
+        @ZZ0008:
 
-no_need_to_display_space:	
-	
-    inx
-    cpx #$09 ; Do we display dot ?
-    bne no_dot_to_display
+        INX
+        cpx #9
+        bne @ZZ0009
+            lda #'.'
+            sta BUFNOM,Y
+            STY TR5
+            iny
+        @ZZ0009:
 
-    lda #'.'
-    sta BUFNOM,y
-    sty TR5 ; store the position of '.'
-    iny
+        cpx #10
+        bne @ZZ0010
+            cmp #' '
+            bne @ZZ0011
+                lda TR5
+                STY TR5
+                tay
+                lda #' '
+                sta BUFNOM,Y
+                ldy TR5
+            @ZZ0011:
+        @ZZ0010:
+        cpx #12
+    bne @ZZ0007
 
-no_dot_to_display:
-	; Suppress dot if there is not an extension
-
-    ; if X=10 then we test if there is an extension
-    cpx #10 
-    bne don_t_test_if_extension_contains_chars
-    cmp #' ' ; Is it a space ? if yes, we delete . of the filename
-    bne don_t_test_if_extension_contains_chars
-	; deleting .
-    lda TR5
-    sty TR5
-    tay
-    lda #' '
-    sta BUFNOM,y ; at this step, filename is "man" instead of "man."
-    ldy TR5
-
-	
-don_t_test_if_extension_contains_chars:
-    cpx #12
-    bne loop12
-loop19:
-    lda #$00
-    sta BUFNOM,y    ; stz can be used in 65C02 but with X register instead of Y
-    sty TEMP_ORIX_1 ; Store the length
-   
-	; reading attributes
-    lda CH376_DATA ; fetching attributes
+    lda CH376_DATA
     cmp #$10
-    bne it_is_a_file
-	; Its a directorty
-    lda #COLOR_FOR_DIRECTORY
-    sta BUFNOM
-    dey ; we remove "."
+    bne @ZZ0012
+        lda #COLOR_FOR_DIRECTORY
+        sta BUFNOM
+        dey
 
+    @ZZ0012:
     lda #$00
-    sta BUFNOM,y
+    sta BUFNOM,Y
+    ;STY TEMP_ORIX_1
 
-    sty TEMP_ORIX_1
-it_is_a_file:
-    ldx #20 ; don't care but we read it, it's the attributes
+    ldx #20
 
-@loop:
-    lda CH376_DATA ; First byte is a
-    dex
-    bpl @loop
+    @ZZ0013:
+        lda CH376_DATA
+        dex
+    bpl @ZZ0013
 
+    lda BUFNOM
+    cmp #'.'
+    beq @ZZ0014
+        lda BUFNOM+1
+        cmp #'.'
+        beq @ZZ0015
+            dec NUMBER_OF_COLUMNS
+            bne @ZZ0016
+                BRK_ORIX XCRLF
+                lda #NUMBER_OF_COLUMNS_LS
+                sta NUMBER_OF_COLUMNS
+            @ZZ0016:
 
-    lda BUFNOM           ; read first char of the file
-    cmp #'.'             ; is it a dot ?
-    beq no_other_space	 ; Yes we don't want to be displayed
-	
-    lda BUFNOM+1         ; read first char of the file
-    cmp #'.'             ; is it a dot ?
-    beq no_other_space	 ; Yes we don't want to be displayed
+            PRINT BUFNOM
 
-    dec NUMBER_OF_COLUMNS
-    bne no_need_to_CRLF
+            ;ldy TEMP_ORIX_1
 
-    BRK_TELEMON XCRLF
-    lda #NUMBER_OF_COLUMNS_LS
-    sta NUMBER_OF_COLUMNS
-  
-no_need_to_CRLF:
-    PRINT BUFNOM
+            @ZZ0017:
+                cpy #13
+                beq @ZZ0018
+                    iny
+                    CPUTC ' '
+            jmp @ZZ0017
 
-	; Add space in order to have a columns
+            @ZZ0018:
+        @ZZ0015:
+    @ZZ0014:
 
-@loop:
-    ldy TEMP_ORIX_1
-    cpy #13   ; FIXME FNAME_LEN ?
-    beq no_other_space
-    iny
-    sty TEMP_ORIX_1
-    CPUTC ' '
-.IFPC02    
-.pc02
-    bra @loop
-.p02    
-.else
-    jmp @loop
-.endif
-
-    
-no_other_space:
-
-    lda #CH376_FILE_ENUM_GO ; 33
+    lda #CH376_FILE_ENUM_GO
     sta CH376_COMMAND
     jsr _ch376_wait_response
-	
-    rts
+rts
+
 optstring:
-    .asciiz "l"
+.BYT 'l',0
+
 .endproc
-
-
 
