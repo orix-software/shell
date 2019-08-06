a8c76f31c10f7372a699e3323f5677ea95342d61
assinie <github@assinie.info>
2019-08-06 15:37:10 +0200

    Revert df.asm

--- src/commands/df.asm
+++ src/commands/df.asm
@@ -1,180 +1,99 @@
-; Conversion: 4484 cycles
-LSB  = RES
-NLSB = LSB+1
-NMSB = NLSB+1
-MSB  = NMSB+1
-
-
-.macro print str, option
-	;
-	; Call XWSTR0 function
-	;
-	; usage:
-	;	PRINT #byte [,TELEMON|NOSAVE]
-	;	PRINT (pointer) [,TELEMON|NOSAVE]
-	;	PRINT address [,TELEMON|NOSAVE]
-	;
-	; Option:
-	;	- TELEMON: when used within TELEMON bank
-	;	- NOSAVE : does not preserve A,X,Y registers
-	;
-	.if (.not .blank({option})) .and (.not .xmatch({option}, NOSAVE)) .and (.not .xmatch({option}, TELEMON) )
-		.error .sprintf("Unknown option: '%s' (not in [NOSAVE,TELEMON])", .string(option))
-	.endif
-
-	.if (.not .blank({option})) .and .xmatch({option}, NOSAVE)
-		.out "Don't save regs values"
-	.endif
-
-	.if .blank({option})
-		pha
-		txa
-		pha
-		tya
-		pha
-	.endif
-
-	.if (.not .blank({option})) .and .xmatch({option}, TELEMON)
-		pha
-		txa
-		pha
-		tya
-		pha
-
-		lda RES
-		pha
-		lda RES+1
-		pha
-	.endif
-
-
-	.if (.match (.left (1, {str}), #))
-		; .out "Immediate mode"
-
-		lda # .right (.tcount ({str})-1, {str})
-		.byte $00, XWR0
-
-	.elseif (.match(.left(1, {str}), {(}) )
-		; Indirect
-		.if (.match(.right(1,{str}), {)}))
-			; .out"Indirect mode"
-
-			lda .mid (1,.tcount ({str})-2, {str})
-			ldy 1+(.mid (1,.tcount ({str})-2, {str}))
-			.byte $00, XWSTR0
-
-		.else
-			.error "-- PRINT: Need ')'"
-		.endif
-
-	.else
-		; assume absolute
-		; .out "Aboslute mode"
-
-		lda #<str
-		ldy #>str
-		.byte $00, XWSTR0
-	.endif
-
-	.if .blank({option})
-		pla
-		tay
-		pla
-		txa
-		pla
-	.endif
-
-	.if (.not .blank({option})) .and .xmatch({option}, TELEMON)
-		pla
-		sta RES+1
-		pla
-		sta RES
-
-		pla
-		tay
-		pla
-		txa
-		pla
-	.endif
-
-.endmacro
-
-
-; .org 1000
 .proc _df
-	jsr _ch376_verify_SetUsbPort_Mount
-	bcc @ZZ0001
+LSB:=TR0 ; FIXME labels and convd
+NLSB:=TR1
+NMSB:=TR2
+MSB:=TR3
 
-		print str_df_columns
-		jsr   _ch376_disk_capacity
+  jsr   _ch376_verify_SetUsbPort_Mount
+  cmp   #$01
+  beq   error
+  jmp   no_error_for_mouting
+error:
+    rts
+no_error_for_mouting:
+  PRINT str_df_columns
+  ;RETURN_LINE
+  jsr   _ch376_disk_capacity
+  lda   TR3
+  jsr   _print_hexa
+  lda   TR2
+  jsr   _print_hexa_no_sharp
+  lda   TR1
+  jsr    _print_hexa_no_sharp
+  lda   TR0
+  jsr   _print_hexa_no_sharp
+  RETURN_LINE
+  rts
+  
+  BRK_TELEMON XCRLF
+ 
 
-		lda TR0
-		sta RES
-		lda TR1
-		sta RES+1
-		lda TR2
-		sta RESB
-		lda TR3
-		sta RESB+1
-		jsr convd
+  lda   #$d2
+  sta   TR0
+  lda   #$02
+  sta   TR1
+  lda   #$96
+  sta   TR2
+  lda   #$49
+  sta   TR3
+  
+  jsr     convd
+  
+  lda     volatile_str
+  AND     #%11110000
+  clc
+  adc     #$30
+  BRK_TELEMON XWR0
 
-		MALLOC 11
-		; FIXME test OOM
-		TEST_OOM_AND_MAX_MALLOC
+  lda     volatile_str
+  AND     #%00001111
+  clc
+  adc     #$30
+  BRK_TELEMON XWR0
+  
+  ; Divide
+  
+    ;12
+  
+  lda     CH376_DATA ; total free sector0
+  lda     CH376_DATA ; total free sector1
+  lda     CH376_DATA ; total free sector2
+  lda     CH376_DATA ; total free sector3
+    
+  
+  
+  lda     CH376_DATA  ; DiskFat type
 
-		sta RESB
-		sty RESB+1
-
-		jsr bcd2str
-		print (RESB)
-		FREE RESB
-
-	@ZZ0001:
-	BRK_ORIX XCRLF
-	rts
 
 
+  rts
+  
+convd:
 
-    convd:
+
         ldx #$04          ; Clear BCD accumulator
         lda #$00
-
     BRM:
-        sta TR0,x        ; Zeros into BCD accumulator
+        sta volatile_str,x        ; Zeros into BCD accumulator
         dex
         bpl BRM
 
         sed               ; Decimal mode for add.
 
         ldy #$20          ; Y has number of bits to be converted
-
     BRN:
         asl LSB           ; Rotate binary number into carry
         rol NLSB
         rol NMSB
         rol MSB
 
-;-------
-; Pour MSB en premier dans BCDA
-;    ldx #$05
-;
-;BRO:
-;    lda BCDA-1,X
-;    adc BCDA-1,X
-;    sta BCDA-1,x
-;    dex
-;    bne BRO
 
-; Pour LSB en premier dans BCDA
-
-BCDA = (TR0-$FB) & $ff ; = $0C
-
+    
         ldx #$fb          ; X will control a five byte addition.
-
     BRO:
-        lda BCDA,x    ; Get least-signficant byte of the BCD accumulator
-        adc BCDA,x    ; Add it to itself, then store.
-        sta BCDA,x
+        lda volatile_str-$fb,x    ; Get least-signficant byte of the BCD accumulator
+        adc volatile_str-$fb,x    ; Add it to itself, then store.
+        sta volatile_str-$fb,x
         inx               ; Repeat until five byte have been added
         bne BRO
 
@@ -183,68 +102,12 @@ BCDA = (TR0-$FB) & $ff ; = $0C
 
         cld               ; Back to binary mode.
         rts               ; And back to the program.
-
-bcd2str:
-	sta RES
-	sty RES+1
-
-	ldx #$04          ; Nombre d'octets à convertir
-	ldy #$00
-;	clc
-
-loop:
-	; BCDA: LSB en premier
-	lda TR0,X
-	pha
-	; and #$f0
-	lsr
-	lsr
-	lsr
-	lsr
-        clc
-	adc #'0'
-	sta (RES),Y
-
-	pla
-	and #$0f
-	adc #'0'
-	iny
-	sta (RES),y
-
-	iny
-	dex
-	bpl loop
-
-	lda #$00
-	sta (RES),y
-	rts
-
+  
+divide_by_1000:
 
 str_df_columns:
     .byte "512-blocks Used Avail. Use% Mounted on",$0d,$0A,0
 
-str_free:
-	.res 11
-
 str_sda1:
     .asciiz "/dev/sda1"
-
 .endproc
-
-; ------------------
-; Temporaire, pour tests
-;RES = *
-
-;LSB = RES
-
-;	.byte  $d2, $02, $96, $00
-
-; Temporaire, pour tests
-;TR0 = *
-
-;BCDA = TR0
-;	.res 5
-
-;str:
-;	.res 11
-
