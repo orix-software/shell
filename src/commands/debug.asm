24619c0a37e28e0e09a5cd37b2cc1126de0f0c7e
jede <jede@oric.org>
2020-03-18 16:17:29 +0100

    Debug command now tries to mount sdcard and key

--- src/commands/debug.asm
+++ src/commands/debug.asm
@@ -25,9 +25,15 @@
     PRINT   str_ch376_check_exist
     jsr     _ch376_check_exist
     jsr     _print_hexa
+    
 	BRK_KERNEL XCRLF
     
-    
+    jsr     mount_sdcard
+    BRK_KERNEL XCRLF
+
+    jsr     mount_key
+    BRK_KERNEL XCRLF
+
     lda #$09
     ldy #$02
   
@@ -36,6 +42,46 @@
     BRK_KERNEL XFREE
     
     rts
+mount_sdcard:
+    lda     #CH376_SET_USB_MODE ; $15
+    sta     CH376_COMMAND
+	lda     #CH376_SET_USB_MODE_CODE_SDCARD
+    sta     CH376_DATA
+    jsr     _ch376_disk_mount
+	cmp 	#CH376_USB_INT_SUCCESS
+	beq 	ok
+    PRINT   str_error_sdcard   	
+    rts
+ok:    
+    PRINT   str_ok_sdcard   
+    rts
+
+mount_key:
+    lda     #CH376_SET_USB_MODE ; $15
+    sta     CH376_COMMAND
+
+    lda     #CH376_SET_USB_MODE_CODE_USB_HOST_SOF_PACKAGE_AUTOMATICALLY
+    sta     CH376_DATA	
+    jsr     _ch376_disk_mount
+	cmp 	#CH376_USB_INT_SUCCESS
+	beq 	ok2
+    PRINT   str_error_key	
+    rts
+ok2:    
+    PRINT   str_ok_key
+    rts    
+    
+
+str_error_sdcard:
+    .asciiz "sdcard mount error !  "
+str_ok_sdcard:
+    .asciiz "sdcard mount OK !  "
+
+str_error_key:
+    .asciiz "key mount error !  "
+str_ok_key:
+    .asciiz "key mount OK !  "
+
 str_ch376:
     .asciiz "CH376 VERSION : "
 str_ch376_check_exist:
