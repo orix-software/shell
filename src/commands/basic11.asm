3378b8ecdc87fd5429efdb51366b39df5c07b944
jede <jede@oric.org>
2020-08-10 01:53:38 +0200

    Work to load .Cnf

--- src/commands/basic11.asm
+++ src/commands/basic11.asm
@@ -7,8 +7,12 @@ basic11_tmp := userzp
 basic11_ptr1 := userzp+1
 basic11_ptr2 := userzp+3
 
+
+;/etc/basic/a/12345678.cnf
+.define basic11_sizeof_max_length_of_conf_file 100 ; used for the path but also for the cnf content
+
 .struct  basic11_struct
-  path                 .res 50   
+  path_conf                 .res basic11_sizeof_max_length_of_conf_file ; /etc/basic/a/12345678.cnf + \0
 .endstruct
 
 
@@ -19,80 +23,97 @@ basic11_ptr2 := userzp+3
     ldx     #$01
     jsr     _orix_get_opt
     ; get parameter
-    bcc     @noparam      ; if there is no args, let's displays all banks
+    bcc     @noparam      ; if there is no args, let's start
 
-    STRCPY  ORIX_ARGV,BUFNOM
-    PRINT ORIX_ARGV
-    rts
-    ; Check if it's a .tap
-    lda     #<ORIX_ARGV
+    lda     #basic11_sizeof_max_length_of_conf_file
+    ldy     #$00
+    BRK_KERNEL XMALLOC
     sta     basic11_ptr1
-
-    lda     #>ORIX_ARGV
-    sta     basic11_ptr1+1
+    sty     basic11_ptr1+1
 
     ldy     #$00
-@L1:
-    lda     (basic11_ptr1),y
-    beq     @found_end
+@L2:    
+    lda     basic_conf_str,y
+    beq     @outcpy
+    sta     (basic11_ptr1),y 
     iny
-    bne     @L1
-    ; overflow open current only
-    jmp     @open_current_cwd
-@found_end:
-    ; we are on the end of the arg
-    dey    ; we reach the last char char
-    lda    (basic11_ptr1),y
-    cmp    #'p'    ; is it p for .taP?
-    bne    @open_current_cwd ; no
-    dey
-    cmp    #'a'    ; is it a for .tAp ?
-    bne    @open_current_cwd ; no
-    dey
-    cmp    #'t'    ; is it a for .Tap ?
-    bne    @open_current_cwd ; no        
-    dey
-    cmp    #'.'    ; is it a for .tap ?
-    bne    @open_current_cwd ; no   
-    ; it's a tape file fill TXTPTR     
-    ; let's reach first / or space
-    dey
-    lda    (basic11_ptr1),y
-    cmp    #' ' ; fill with space
-    beq    @fill_txt_ptr
+    bne     @L2
 
-
-@fill_txt_ptr:
+@outcpy:
+    ; do strcat
+    ; get letter
+    ldx     #$01      ; skip double quote
+    lda     ORIX_ARGV,x
+    sta     (basic11_ptr1),y
     iny
-    lda    basic11_ptr1
-    sta    basic11_TXTPTR
-
-    lda    basic11_ptr1+1
-    sta    basic11_TXTPTR+1   
-
-    tya
-    clc
-    adc    basic11_TXTPTR
-    bcc    @do_not_inc
-    inc    basic11_TXTPTR+1   
-@do_not_inc:
-    sta    basic11_TXTPTR
-
-    lda    basic11_TXTPTR
-    ldy    basic11_TXTPTR+1
-    BRK_KERNEL XWSTR0 
-
-
-@open_current_cwd:
-    ; Open args
-    lda     #<ORIX_ARGV
-    ldx     #>ORIX_ARGV
+    sta     (basic11_ptr1),y ; get another letter
     
+    inx
+@L3:    
+    lda     ORIX_ARGV,x
+    cmp     #$22 ; " char
+    beq     @outstrcat
+    cmp     #$00
+    beq     @outstrcat
+    sta     (basic11_ptr1),y ; get another letter
+    iny
+    inx     
+    bne     @L3
+@outstrcat:
+    ; concat .cnf
+    lda     #'.'
+    sta     (basic11_ptr1),y
+    iny
+    lda     #'c'
+    sta     (basic11_ptr1),y
+    iny
+    lda     #'n'
+    sta     (basic11_ptr1),y    
+    iny
+    lda     #'f'
+    sta     (basic11_ptr1),y    
+    iny
+    lda     #$00 ; store end of string
+    sta     (basic11_ptr1),y    
+
+
+    ldx     basic11_ptr1+1
+    lda     basic11_ptr1
     ldy     #O_RDONLY
 
     BRK_KERNEL XOPEN ; open current
+    cpy     #$00
+    bne     @parsecnf ; not null then  start because we did not found a conf
+    cmp     #$00
+    beq     @noparam_free ; not null then  start because we did not found a conf
+@parsecnf:
+  ; define target address
+    lda     basic11_ptr1
+    sta     PTR_READ_DEST
+    
+    lda     basic11_ptr1+1
+    sta     PTR_READ_DEST+1
 
-    jmp     @start
+  ; We read the file with the correct
+    lda     #<basic11_sizeof_max_length_of_conf_file
+    ldy     #>basic11_sizeof_max_length_of_conf_file
+  ; reads byte 
+    BRK_KERNEL XFREAD
+    BRK_KERNEL XCLOSE
+
+    ; We found a conf
+
+    rts
+
+    ;STRCPY  ORIX_ARGV,BUFNOM
+    ;PRINT ORIX_ARGV
+    ;rts
+    ; Check if it's a .tap
+ @noparam_free:  
+    lda     basic11_ptr1
+    ldy     basic11_ptr1+1
+
+    BRK_KERNEL XFREE
 
 @noparam:
     ; Get current pwd and open
@@ -145,4 +166,6 @@ copy:
     lda     #ATMOS_ID_BANK
     sta     VIA2::PRA
     jmp     $F88F ; NMI vector of ATMOS rom
+basic_conf_str:
+    .asciiz "/etc/basic/"    
 .endproc
