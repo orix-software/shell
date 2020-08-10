fb814a396459d76b7fcdc184ddb0176067d230c6
jede <jede@oric.org>
2020-08-11 01:25:01 +0200

    prepare exec

--- src/commands/exec.asm
+++ src/commands/exec.asm
@@ -2,44 +2,16 @@
 .export _exec
 
 .proc _exec
-    lda bash_struct_command_line_ptr
-    lda bash_struct_command_line_ptr+1
-    ldy     #$05 ; Now we remove exec word and space
- @loop:
-     lda    (bash_struct_command_line_ptr),y
-     beq     @out
-     dey
-     dey
-     dey
-     dey
-     dey
-     sta     (bash_struct_command_line_ptr),y
-
-     iny
-     iny
-     iny
-     iny
-     iny
-     iny
-     bne     @loop
- @out:
-     dey
-     dey
-     dey
-     dey
-     dey
-     sta     (bash_struct_command_line_ptr),y
-
-    ldy     bash_struct_command_line_ptr+1
-    lda     bash_struct_command_line_ptr
-    clc
-    adc     #$04
-    bcc     @S1
-    iny
-@S1:    
+    ldx     #$01
+    jsr     _orix_get_opt
+    bcc     @usage
+    lda     #<ORIX_ARGV
+    ldy     #>ORIX_ARGV
+@L1:    
+    BRK_KERNEL XEXEC
     
     
-    BRK_KERNEL($63) ; Exec
+@usage:
 
     rts
 
