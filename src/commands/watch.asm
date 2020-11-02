aee982abf8dfa7938e8f5d98c8bfff1f16204705
jede <jede@oric.org>
2020-11-03 00:59:48 +0100

    watch almost works

--- src/commands/watch.asm
+++ src/commands/watch.asm
@@ -2,21 +2,27 @@
 
 save_mainargs_ptr  := userzp
 watch_ptr1         := userzp+2
+watch_ptr2         := userzp+4
 
 .proc _watch
     ldx     #$01
     jsr     _orix_get_opt
-    bcc     @usage
+    
 
     MALLOC_AND_TEST_OOM_EXIT 100 
     sta     save_mainargs_ptr
     sty     save_mainargs_ptr+1
     
+    ldx     #$01
+    jsr     _orix_get_opt
+    bcc     @usage
+
     lda     #<ORIX_ARGV
-    ldy     #>ORIX_ARGV
-    
     sta     watch_ptr1
-    sty     watch_ptr1+1
+    lda     #>ORIX_ARGV
+    sta     watch_ptr1+1
+    
+
 
     ; copy command
     ldy     #$00
@@ -29,49 +35,81 @@ watch_ptr1         := userzp+2
 @S3:
     sta     (save_mainargs_ptr),y
 
-
-
+    lda     save_mainargs_ptr
+    ldy     save_mainargs_ptr+1
+    BRK_KERNEL XWSTR0
+    SWITCH_OFF_CURSOR
 @L1:
     asl     KBDCTC
     bcc     @no_ctrl
+
+    SWITCH_ON_CURSOR
+
+    
     rts
 
-@no_ctrl:    
+@no_ctrl:
+    ; reset prompt position
+    lda     #<(SCREEN)
+    sta     ADSCR
+    lda     #>(SCREEN)
+    sta     ADSCR+1
+
+    lda     #$00
+    sta     SCRDY
+
+    ; reset display position
+    ldx     #$00
+    stx     SCRY
+    stx     SCRX
+
+    lda     #<SCREEN                                ; Get position screen
+    ldy     #>SCREEN
+    sta     RES
+    sty     RES+1
+
+    ldy     #<(SCREEN+SCREEN_XSIZE*SCREEN_YSIZE)
+    ldx     #>(SCREEN+SCREEN_XSIZE*SCREEN_YSIZE)
+    lda     #' '
+    BRK_TELEMON XFILLM                              ; Calls XFILLM : it fills A value from RES address and size of X and Y value
+
     lda     save_mainargs_ptr
     ldy     save_mainargs_ptr+1
 
     BRK_KERNEL XEXEC
+    cmp     #ENOENT
+    beq     @notfound
+    jsr     @wait
     jmp     @L1
+
+@notfound:
+    lda     save_mainargs_ptr
+    ldy     save_mainargs_ptr+1
+    BRK_KERNEL XWSTR0 
+    PRINT str_not_found
+    rts
 
 @usage:    
     rts
-    PRINT   str_argc
-
-    BRK_KERNEL $2C ; XMAINARGS
-    ; Return in A & Y struct
-    ; save ptr
-    sta save_mainargs_ptr
-    sty save_mainargs_ptr+1
+ 
 
 
-    BRK_KERNEL $2D ; XGETARGC
-    PRINT_BINARY_TO_DECIMAL_16BITS 2
-    RETURN_LINE 
-    PRINT str_param
+@wait:
+    ldy     #$00
     ldx     #$00
-    lda     save_mainargs_ptr
-    ldy     save_mainargs_ptr+1
-    BRK_KERNEL $2E ; XGETARGV
-    BRK_KERNEL XWSTR0 
-    RETURN_LINE 
-    
-    ldx     #$01
-    lda     save_mainargs_ptr
-    ldy     save_mainargs_ptr+1
-    BRK_KERNEL $2E ; XGETARGV
-    BRK_KERNEL XWSTR0 
-    RETURN_LINE 
-
+@lwait:
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    inx
+    bne     @lwait
+    iny
+    bne     @lwait
     rts
 str_argc:
     .asciiz "Argc: "  
