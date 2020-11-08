ba1588c03310986fbe71b913b2bda13eb03fcc0c
jede <jede@oric.org>
2020-11-08 01:47:51 +0100

    Switch uname to new XMAINARGS

--- src/commands/uname.asm
+++ src/commands/uname.asm
@@ -1,13 +1,34 @@
 .proc _uname
-    ldx     #$01
-    jsr     _orix_get_opt
-    STRCPY  ORIX_ARGV,BUFNOM
- 
-    lda     BUFNOM
+    uname_mainargs_ptr := userzp
+
+    uname_mainargs_argv := userzp+3
+    uname_mainargs_argc := userzp+2
+
+
+    BRK_KERNEL XMAINARGS
+    sta   uname_mainargs_ptr
+    sty   uname_mainargs_ptr+1
+    stx   uname_mainargs_argc
+    
+    ldx   #$01
+    lda   uname_mainargs_ptr
+    ldy   uname_mainargs_ptr+1
+
+    BRK_KERNEL XGETARGV
+    sta   uname_mainargs_argv
+    sty   uname_mainargs_argv+1
+
+    lda     uname_mainargs_argc
+    cmp     #$01
     beq     no_param
+
+    ldy     #$00
+    lda     (uname_mainargs_argv),y    
     cmp     #'-'
     bne     error
-    lda     BUFNOM+1
+
+    iny     
+    lda     (uname_mainargs_argv),y    
     cmp     #'a'
     bne     error
     PRINT   str_os
