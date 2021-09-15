0f6c33b08b7090fd39378d131486c23c11da2fa3
jede <jede@oric.org>
2021-09-16 00:10:04 +0200

    env and banks improved

--- buildr64.sh
+++ buildr64.sh
@@ -1,6 +1,5 @@
-make
-cp shellus.rom k2021-2.r64
-cat ../../kernel/develop/basicus1.rom >> k2021-2.r64
-cat ../../kernel/develop/kernelus.rom >> k2021-2.r64
-cat ../../empty-rom/empty-rom.rom  >> k2021-2.r64
-cp k2021-2.r64 /s/devus.r64
+cp shellus.rom k2021-4.r64
+cat ../../kernel/develop/basicus1.rom >> k2021-4.r64
+cat ../../kernel/develop/kernelus.rom >> k2021-4.r64
+cat ../../empty-rom/empty-rom.rom  >> k2021-4.r64
+cp k2021-4.r64 /s/devus.r64
