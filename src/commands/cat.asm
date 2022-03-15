e064d5ccb55c751af342fb4bda74e4927fdfc075
jedeoric <plifplouf78@hotmail.com>
2022-03-15 22:57:10 +0100

    fix tab cat

--- src/commands/cat.asm
+++ src/commands/cat.asm
@@ -36,58 +36,58 @@
     cmp     #$FF
     bne     @readfile
 
-    print (cat_save_ptr_arg),NOSAVE
+    print   (cat_save_ptr_arg),NOSAVE
 
-    print str_not_found,NOSAVE
+    print   str_not_found,NOSAVE
     rts
 
 @print_usage:
 @cat_error_param:
-    print txt_usage,NOSAVE
+    print     txt_usage,NOSAVE
     rts
 
 @readfile:
 
-    lda #$FF
+    lda     #$FF
     tay
-    jsr _ch376_set_bytes_read
+    jsr     _ch376_set_bytes_read
     ; Renvoie CH376_USB_INT_SUCCESS ($14) si le fichier est vide, CH376_USB_INT_DISK_READ ($1D) si ok
 
   @loop:
-    cmp #CH376_USB_INT_DISK_READ
-    bne @finished
+    cmp     #CH376_USB_INT_DISK_READ
+    bne     @finished
 
-    lda #CH376_RD_USB_DATA0
-    sta CH376_COMMAND
-    lda CH376_DATA
-    sta userzp
+    lda     #CH376_RD_USB_DATA0
+    sta     CH376_COMMAND
+    lda     CH376_DATA
+    sta     userzp
     ; Tester si userzp == 0?
 
   @read_byte:
-    lda CH376_DATA
-    cmp #$0A
-    bne @autre
+    lda     CH376_DATA
+    cmp     #$0A
+    bne     @autre
 
     BRK_TELEMON XCRLF
-    bne @next    ; ACC n'est pas modifié par XCRLF, donc saut inconditionnel
+    bne     @next    ; ACC n'est pas modifié par XCRLF, donc saut inconditionnel
 
   @autre:
-    cmp #$0D
-    beq @next
+    cmp     #$0D
+    beq     @next
 
     BRK_TELEMON XWR0
 
   @next:
-    dec userzp
-    bne @read_byte
+    dec     userzp
+    bne     @read_byte
 
-    lda #CH376_BYTE_RD_GO
-    sta CH376_COMMAND
-    jsr _ch376_wait_response
+    lda     #CH376_BYTE_RD_GO
+    sta     CH376_COMMAND
+    jsr     _ch376_wait_response
 
     ; _ch376_wait_response renvoie 1 en cas d'erreur et le CH376 ne renvoie pas de valeur 0
     ; donc le bne devient un saut inconditionnel!
-    bne @loop
+    bne     @loop
 
     ; Tester si ACC==1 pour détecter une éventuelle erreur?
 
